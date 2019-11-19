04de68c410b28129ea74a9d6fa5c87971ef5093b
ctola <christian.tola@truextend.com>
2019-11-19 11:52:38 -0400

    Add semantic ui and scss support

--- client/src/shared/components/layout/Header.js
+++ client/src/shared/components/layout/Header.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
 
-const Header = () => <div>Header</div>;
+import './Header.scss';
+
+const Header = () => (
+  <header>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={4}>
+              <div className="logo"><h1>Pizza App</h1></div>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <div className="navbar">
+                <ul className="ui grid">
+                  <li className="four wide column">
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li className="four wide column">
+                    <Link to="/toppings">Toppings</Link>
+                  </li>
+                  
+                </ul>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </header>
+);
 
 export default Header;
